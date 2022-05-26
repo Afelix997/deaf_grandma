9c3b1446b6f3eea29f5ca78cd6847d050da8afbd
afelix997 <afelix2013@gmail.com>
2022-05-25 19:15:04 -0700

    new

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,21 @@
-function deafGrandma() {
-hello
+function deafGrandma(){	
+    let byes = 0;
+    while (byes < 2){
+      let input = window.prompt('HEY KID!');
+        if (input == ''){	
+            window.alert('WHAT?!');
+        } else if (input !== input.toUpperCase()){	
+            window.alert('SPEAK UP, KID!');
+        } else if (input === input.toUpperCase() && input !== 'GOODBYE!'){	
+            window.alert('NO, NOT SINCE 1946!');  
+        } else if (input === 'GOODBYE!' && byes < 1){	
+            window.alert('LEAVING SO SOON?');	
+            byes++;  
+        } else if (input === 'GOODBYE!'){	
+            window.alert('LATER, SKATER!');	
+            break;
+        }
+    }
 }
-
+  
 deafGrandma();
