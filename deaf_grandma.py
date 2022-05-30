75f45a8fc3cee9e0c46a67cf94724b4b38c470c3
afelix997 <afelix2013@gmail.com>
2022-05-29 19:05:35 -0700

    answer

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,18 @@
 def deaf_grandma():
-
-
+    byes=0
+    while byes < 2:
+        user_input=input('HEY KID!')
+        if user_input == "":
+            print("WHAT?")
+        elif not user_input.isupper():
+            print('SPEAK UP KID!')
+        elif user_input.isupper() and user_input != 'GOODBYE!':
+            print('NO NOT SINCE 1946!')
+        elif user_input == 'GOODBYE!' and byes < 1:
+            print('LEAVING SO SOON?')
+            byes +=1
+        elif user_input == 'GOODBYE!':
+            print('LATER SKATER!')
+            break
 
 deaf_grandma()
